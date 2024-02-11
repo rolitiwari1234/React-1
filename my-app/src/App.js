3a0631fc30f54c2fc15aee89c79a8631a914095b
rolitiwari1234 <aniltiwari9917975244@gmail.com>
2024-02-11 21:18:24 +0530

    useMemo Hook

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,28 @@
-import React,{PureComponent} from 'react'
+import React, { useState ,useMemo} from 'react'
 import './App.css';
-import Pure from './Pure';
 
-class App extends React.Component{
-  constructor(){
-    super();
-    this.state= {
-      count:1
-    }
-  }
-  render(){
-    console.warn("check-rerending")
+function App(){
+  const [count,setCount]=useState(0)
+  const [item,setItem]= useState(10)
+
+  const  multiCountMemo = useMemo(function multiCount()
+  {
+   console.warn("multiCount")
+   return count*2
+  },[count])
+
     return (
       <div className="App">
-        <Pure count = {this.state.count}/>
-      
-       <button onClick={()=>this.setState({count:this.state.count+1})}
-       >update count</button>
-      </div>
-    );
-  }
-}
+        <h1>useMemo hook in react</h1>
 
-  
+        <h2>count : {count}</h2>
+        <h2>item: {item}</h2>
+        <h2> {multiCountMemo} </h2>
 
-
+        <button  onClick=  {()=>setCount(count+1)} >update count</button>
+        <button onClick = {()=>setItem(item*10)} >update item</button>
+       </div>
+    )
+    };
 
 export default App;
