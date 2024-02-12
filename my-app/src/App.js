a22e298fc4c1b5d337cfdd3f36cb2ce091ac8a69
rolitiwari1234 <aniltiwari9917975244@gmail.com>
2024-02-12 21:22:36 +0530

    uncontrolled component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,28 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 import './App.css';
 
 
 function App(){
-   let [val,setVal]= useState("000")
-    let [item,setItem] = useState("")
+    let inputRef = useRef(null)
+    let inputRef2 = useRef(null)
+
+    function submitForm(e){
+      
+      console.warn("input field  1 value :",inputRef.current.value)
+      console.warn("input field 2 in value :",inputRef2.current.value)
+      let input3 = document.getElementById("input3").value
+      console.warn("input field  3 value :",input3)
+    }
     return (
       <div className="App">
-       <h1>controlled component</h1>
-      <input type = "text" value = {val} onChange={(e)=>setVal(e.target.value)}/>
-      <input type = "text" value = {item} onChange = {(e)=>setItem(e.target.value)}/>
-      <h3>value {val}</h3>
-      <h3>item {item}</h3>
+       <h1>Uncontrolled component</h1>
+      <form onSubmit = {submitForm}>
+        <input ref = {inputRef} type = "text" /><br/><br/>
+        <input ref = {inputRef2} type = "text" /><br/><br/>
+        <input id = "input3" type = "text"/><br/><br/>
+      
+        <button>Submit</button>
+        </form>
       </div>
     )
     }
