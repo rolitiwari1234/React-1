fa5ec19f2633adf8ffd78343f7e852091215d765
rolitiwari1234 <aniltiwari9917975244@gmail.com>
2024-02-12 13:54:27 +0530

    controlled component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,17 @@
-import React, { useRef} from 'react'
+import React, { useState } from 'react'
 import './App.css';
-import Forward from './Forward';
+
 
 function App(){
-  let inputRef = useRef(null)
-    function updateInput()
-    {
-      inputRef.current.value = "1000"
-      inputRef.current.style.color = "red"
-      inputRef.current.focus()
-    }
-   
+   let [val,setVal]= useState("000")
+    let [item,setItem] = useState("")
     return (
       <div className="App">
-       <h1>forwardref in react</h1>
-       <Forward ref = {inputRef}/>
-       <button onClick = {updateInput}>update inputbox</button>
-      
+       <h1>controlled component</h1>
+      <input type = "text" value = {val} onChange={(e)=>setVal(e.target.value)}/>
+      <input type = "text" value = {item} onChange = {(e)=>setItem(e.target.value)}/>
+      <h3>value {val}</h3>
+      <h3>item {item}</h3>
       </div>
     )
     }
