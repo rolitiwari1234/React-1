7053ba5cfd3d1485e10279aac2acdb0369f7d895
rolitiwari1234 <aniltiwari9917975244@gmail.com>
2024-02-18 17:00:39 +0530

    React Routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,30 @@
-import { BrowserRouter, Route,Routes,Navigate} from 'react-router-dom'
 import './App.css';
-import Home from './components/Home';
-import About from './components/About'
-import Navbar from './components/Navbar';
-import User from './components/User';
-import Filter from './components/Filter';
-import Contact from './components/Contact';
-import Company from './components/Company';
-import Channel from './components/Channel'
-import Other from './components/Other';
-import Login from './components/Login';
-import Protected from './components/Protected';
+import React from 'react'
+import {Link, Route, BrowserRouter as Router } from "react-router-dom"
 
 function App(){
-
-    return (
+return (
       <div className="App">
-        <BrowserRouter>
-   <Navbar/>
-      
-      <Routes>
-         <Route path = "/" element = {<Protected Component = {Home}/>}/> 
-         <Route path = "/about" elememt = {<About />}/>
-         <Route path = "/user " element = {<User/>}/>
-         <Route path = "/filter" element =  {<Filter/>}/>
-        <Route path = "/" element = {<Navigate to = "/"/>}/> 
-         <Route path ="contact" element = {<Contact/>}/>
-         <Route path = "company" element = {<Company/>}/>
-         <Route path = "channel" element = {<Channel/>}/>
-         <Route path = "/other" element = {<Other/>}/>
-         <Route path = "/login" element = {<Login/>}/>
-           </Routes>
-      </BrowserRouter>
-      </div>
-    )
+        <Router>
+      <Link to = "/home">Home page</Link>
+      <Link to = "/about">About page</Link>
+      <Route path = "/home" > <Home/> </Route>
+      <Route path = "/about"><About/></Route>   
+    </Router>
+       </div>) }
+function Home()
+    {
+      return(
+        <div>
+          <h1>Home page</h1>
+          <p>This is a home page website .</p>
+        </div>)}
+    function About (){
+      return(
+        <div>
+          <h1>About page</h1>
+          <p>This is a about page website .</p>
+        </div>
+      )
     }
-
-export default App;
+    export default App;
