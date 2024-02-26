47e6512721d0b277d1796e2bbcb1ccb6130fa0bb
rolitiwari1234 <aniltiwari9917975244@gmail.com>
2024-02-26 12:43:45 +0530

    Routing setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,39 @@
-import { BrowserRouter, Route,Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route,Routes ,Link, BrowserRouter} from 'react-router-dom'
 import './App.css';
 import React from 'react'
-import Home from './components/Home';
-import About from './components/About'
-import Blog from './components/Blog'
-import Contact from './components/Contact'
-import Layout from './components/Layout';
+
 function App(){
   return(
   <div className='App'>
-    <BrowserRouter>
-    <Routes >
+
     
-    <Route path = "/home" element = {<Home />} />
-    <Route path = "/about" element = {<About />} />
-    <Route path = "/blog" element = {<Blog />} />
-    <Route path = "/layout" element = {<Layout />} />
-    <Route path = "/contact" elemnet = {<Contact />} />
-   
+    <BrowserRouter>
+  
+    <Link to = "/home">home</Link><br />
+    <Link to = "/about">about</Link>
+    <Routes>
+      <Route path = "home" element = {<Home />} />
+      <Route path = "about" element = {<About />} />
     </Routes>
     </BrowserRouter>
   </div>)
 }
-
-
+function Home(){
+  return(
+    <div>
+      <h1>Home page</h1>
+      <p>This is  home page of website .</p>
+    </div>
+  )
+}
+function About (){
+  return(
+    <div>
+      <h1>about page</h1>
+      <p>This is a about page of website </p>
+    </div>
+  )
+}
      
 
 export default App;
