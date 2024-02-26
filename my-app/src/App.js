56893654fadcb114da8ec9497badc962bf236a39
rolitiwari1234 <aniltiwari9917975244@gmail.com>
2024-02-26 18:05:08 +0530

    Page 404

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,38 @@
-import { BrowserRouter as Router, Route,Routes ,Link, BrowserRouter} from 'react-router-dom'
+import { BrowserRouter as Router, Route,Routes ,Link, BrowserRouter } from 'react-router-dom'
 import './App.css';
 import React from 'react'
-import Nav from './Nav';
-import Home from './Home'
-import About from './About'
+import Page404 from './Page404'
 
 function App(){
   return(
   <div className='App'>
- 
-  <Nav />
+   
+    <Link to = "/">home</Link><br />
+    <Link to = "/about">about</Link><br />
+    <Link to = "/login">page404</Link>
   
     <Routes>
-    
+   
+    <Route path = "/" exact = {true} element = {<Home />} />
       <Route path = "/about" element = {<About />} />
-      <Route path = "/"  exact = {true} element = {<Home />} />
+     <Route path = "/*" element = {<Page404 />} />
+    
     </Routes>
-      
+    
   </div>)
+}
+function Home(){
+  return(
+    <div>
+      <h1>home page</h1>
+    </div>
+  )
+}
+function About (){
+  return(
+    <div>
+      <h1>about page</h1>
+    </div>
+  )
 }
 export default App;
---
+++ my-app/src/Page404.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Page404(){
+    return(
+        <div>
+            <h1>Page not found</h1>
+        </div>
+    )
+}
+export default Page404;
