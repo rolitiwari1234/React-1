9576f932f6107785ab84648ea6f5355e5f7ee7c0
rolitiwari1234 <aniltiwari9917975244@gmail.com>
2024-02-28 16:50:07 +0530

    dynamic routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router ,Link, Route,Routes} from 'react-router-dom';
+import {  Link, Route,Routes } from 'react-router-dom';
 import './App.css';
 import React from 'react'
 import Dynamic  from './Dynamic';
@@ -8,20 +8,20 @@ function App(){
   const dynamics = [
     {id:"1",name:"roli", email:"roli@gmail.com",},
     { id:"2",name:"yashi",email:"yashi@gmail.com",},
-    {id:"3",name:"yash",email:"yash@gmail.com"},]
+    {id:"3",name:"abc",email:"abc@gmail.com"},]
 
   return(
   <div className='App'>
-    
    <h1> React Dynamic Routing</h1>
+       
    {
-    dynamics.map((item)=>
-    <div><Link to = {"/dynamics/" + item.id}><h3>{item.name}</h3></Link></div>
-    )
+       dynamics.map((item)=>
+       <div><Link to = {"/dynamics/" + item.id +"/"+  item.name + "/" +item.email} ><h3>{item.name}</h3></Link></div>
+      )
    }
-  <Routes>
-    <Route path = "/dynamics/:id" element = { <Dynamic />}> </Route>
-  </Routes>
-    </div>
+     <Routes>
+       <Route path = "/dynamics/:id/:name/:email" element = { <Dynamic />}> </Route>
+    </Routes>
+     </div>
     )}
 export default App;
--- my-app/src/Dynamic.js
+++ my-app/src/Dynamic.js
@@ -1,11 +1,17 @@
-import {} from 'react-router-dom'
+import React from 'react'
+import { useParams } from 'react-router-dom';
 
-function Dynamic (){
-   
+function Dynamic ()
+{
+     const params = useParams()
+    
+     console.warn(params)
     return(
         <div>
-            <h1>this is dynamic page </h1>
+            <h1>this is dynamics no . {params.id}</h1>
+            <h2>dynamics name {params.name} </h2>
+           
         </div>
     )
 }
-export default Dynamic;
+export default Dynamic; 
