e082e3e85f8a1d0fc13468ef0ad079fd4319948c
rolitiwari1234 <aniltiwari9917975244@gmail.com>
2024-02-29 14:01:16 +0530

    Get Api method

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,41 @@
 
-import {  Link, Route,Routes } from 'react-router-dom';
+
 import './App.css';
-import React from 'react'
-import Dynamic  from './Dynamic';
+import React ,{useEffect, useState}from 'react'
+
 
 function App(){
-  const dynamics = [
-    {id:"1",name:"roli", email:"roli@gmail.com",},
-    { id:"2",name:"yashi",email:"yashi@gmail.com",},
-    {id:"3",name:"abc",email:"abc@gmail.com"},]
-
+  const [data,setdata] = useState([])
+  useEffect(()=>{
+   fetch("https://jsonplaceholder.typicode.com/todos")
+   .then((result)=>{
+    result.json().then((response)=>{
+      //  console.log("result",response)
+       setdata(response)
+    })
+   }
+  
+   
+  )},[])
+   console.log(data)
   return(
   <div className='App'>
-   <h1> React Dynamic Routing</h1>
-       
-   {
-       dynamics.map((item)=>
-       <div><Link to = {"/dynamics/" + item.id +"/"+  item.name + "/" +item.email} ><h3>{item.name}</h3></Link></div>
-      )
-   }
-     <Routes>
-       <Route path = "/dynamics/:id/:name/:email" element = { <Dynamic />}> </Route>
-    </Routes>
-     </div>
-    )}
+   <h1>   API routing</h1>
+        <table border = "1">
+          <tr>
+            <td>userId</td>
+            <td>Id</td>
+            <td>title</td>
+          </tr>
+          {
+            data.map((item)=>
+            <tr>
+              <td>{item.userId}</td>
+              <td>{item.id}</td>
+              <td>{item.title}</td>
+            </tr>)
+          }
+        </table> 
+     </div> 
+  )}
 export default App;
