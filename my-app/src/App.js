8d4a1ac673db9c4f45894f2e83c02355c6db4159
rolitiwari1234 <aniltiwari9917975244@gmail.com>
2024-03-06 21:30:30 +0530

    State with Object

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,18 @@
 import './App.css'
-import React, { useState } from'react'
-import Preprops from './Preprops';
+import React from 'react'
 
 function App(){
-  const [count,setCount] = React.useState(0)
-
-  
+  const [data,setData] = React.useState({name:"roli",age:20})
   return(
     <div className='App'>
-     
-     <Preprops count = {count} />
-     <button onClick = {()=>setCount(Math.floor(Math.random()*10)) }>update count</button>
+      <h1>State Object with Hooks</h1>
+      <input type = "text" placeholder='enter your name' value = {data.name}
+      onChange={(e)=>setData({...data,name:e.target.value})}/><br/><br/>
+
+      <input type = "text" placeholder ="enter your age" value = {data.age}
+      onChange = {(e)=>setData({...data,age:e.target.value})}/>
+      <h1>{data.name}</h1>
+      <h1>{data.age}</h1>
     </div>
   )
 }
