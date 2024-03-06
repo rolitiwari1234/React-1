2ced0490986b6811774e55b7889174b4513202e6
rolitiwari1234 <aniltiwari9917975244@gmail.com>
2024-03-06 18:28:45 +0530

    Previous state in functional component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,103 +1,34 @@
-import React,{useEffect,useState} from 'react'
 import './App.css'
+import React, { useState } from'react'
 
 function App(){
 
-  const [user,setUser] = useState([]);
-  const [name,setName] = useState("");
-  const [id,setId] = useState("")
-  const[username,setUsername] = useState("")
-  const[email,setEmail] = useState("")
+  const[count,setCount]= useState(1)
+    function updateCounter(){
 
-  useEffect(()=>{
-    getList()
-  },[] )
- 
-  function getList(){
-      fetch("https://jsonplaceholder.typicode.com/users").then((result)=>{
-        result.json().then((resp)=>{
-          // console.warn(resp)
-          setUser(resp)
+  //   let rand = Math.floor(Math.random()*10)
+  //  setCount((pre)=>{
+  //   console.warn(pre)
+  //   if(pre < 5){
+  //     alert("low value")
+  //   }
+  //   return rand
+  // })
 
-          setName(resp[0].name)
-          setId(resp[0].id)
-          setUsername(resp[0].username)
-          setEmail(resp[0].email)
-
-        })
-      })}
-      function deleteUser(id){
-       fetch(`https://jsonplaceholder.typicode.com/users/${id}`,{
-        method:'DELETE'
-       }).then((result)=>{
-        result.json().then((resp)=>{
-          console.warn(resp)
-          getList()
-        })
-       })
-      }
-    
-  function selectUser(id){
-    console.warn("function call",user[id-1])
-
-     let item = user[id-1]
-    setName(item.name)
-    setId(item.id)
-    setUsername(item.username)
-    setEmail(item.email)
-
-  }
-  function updateUser(){
-    let item ={name,id,username,email}
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`,{
-      method:"PUT",
-      headers:{
-        'Accept':'application/json',
-        'Content-type':'application/json'
-      },
-      body:JSON.stringify(item)
-    }).then((result)=>{
-      result.json().then((resp)=>{
-        console.warn(resp)
-        getList()
-      })
-    })
+    for (let i = 0;i <5;i++)
+    {
+     setCount((pre)=>
+     {
+      return pre+1
+     }
+     )
+    }
   }
   return(
-    <div className="App">
-    <h2>Delete Data with Api Call</h2>
-      <table border = "1" >
-        <tbody>
-        <tr>
-        <td>id</td>
-          <td>Name</td>
-          <td>username</td>
-          <td>email</td>
-          <td>optations</td>
-         </tr>
-        {
-          user.map((item,i)=>
-          <tr key = {i}>
-          
-            <td>{item.id}</td>
-            <td>{item.name}</td>
-            <td>{item.username}</td>
-            <td>{item.email}</td>
-            <button onClick={()=>deleteUser(item.id)}>delete</button>
-           <td><button onClick={()=>selectUser(item.id)}>update </button></td>
-          </tr>)
-        }
-        </tbody>
-      </table>
-      <div >
-        <input type  = "text" value = {name} onChange={(e)=>setName(e.target.value)}/><br /><br />
-        <input type = "text" value = {id} onChange={(e)=>setId(e.target.value)}/><br /><br/>
-        <input type = "text" value = {username} onChange = {(e)=>setUsername(e.target.value)}/><br/><br/>
-        <input type = "text" value = {email} onChange = {(e)=>setEmail(e.target.value)}/><br/><br/>
-        <button onClick={updateUser}>update user</button>
+    <div className='App'>
+      <h1>Previoue state {count }</h1>
+      <button onClick={updateCounter}>click me update counter</button>
     </div>
-    </div>
-
   )
 }
 export default App;
