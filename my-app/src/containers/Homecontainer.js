422955f3b6f92c231b57f9f61deeb4f89fd8a19b
rolitiwari1234 <aniltiwari9917975244@gmail.com>
2024-03-09 20:50:17 +0530

    Container-mapDispatchToProps and mapStateToProps

--- my-app/src/containers/Homecontainer.js
+++ my-app/src/containers/Homecontainer.js
@@ -1,4 +1,16 @@
+import {connect} from 'react-redux'
+import {addToCart} from '../services/Actions/action'
 import HomeRedux from "../components/HomeRedux";
 
 
-export default HomeRedux;
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = display => ({
+    addToCartHandler :data => display(addToCart(data))
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(HomeRedux)
+
+// export default HomeRedux;
